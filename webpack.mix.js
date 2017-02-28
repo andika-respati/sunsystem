ce608a4d9c05e0a15fcb577a431e43b6ca2b1eac
Andika Respati <ares.area@gmail.com>
2017-03-01 05:09:06 +0700

    3 1 2017 5 08

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,17 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.styles([
+    'bower_components/bootstrap/dist/css/bootstrap.css',
+    'bower_components/bootstrap/dist/css/bootstrap-theme.css',
+    'bower_components/font-awesome/css/font-awesome.css',
+    'bower_components/select2/dist/css/select2.css'
+],'public/css/vendor.css');
+
+mix.scripts([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/bootstrap/dist/js/bootstrap.js',
+    'bower_components/d3/d3.js',
+    'bower_components/select2/js/select2.full.js'
+],'public/js/vendor.js');
